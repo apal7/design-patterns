3c0e1b96c71ef2bd02dae095c1cf8c636958c5ce
Andy <no@email>
2019-02-08 17:43:01 -0300

    Factory Pattern - Images

---
+++ creational-patterns/factory/src/main/java/com/apal/designpatterns/factory/FactoryApplication.java
@@ -0,0 +1,14 @@
+package com.apal.designpatterns.factory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FactoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FactoryApplication.class, args);
+	}
+
+}
+
