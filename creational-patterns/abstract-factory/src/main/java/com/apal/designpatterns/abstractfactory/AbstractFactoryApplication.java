2c9999744ef585640cc531272ac398e9bfb3a6ff
Andy <no@email>
2019-02-08 22:45:43 -0300

    README.md: Abstract Factory | Folder Structure

---
+++ creational-patterns/abstract-factory/src/main/java/com/apal/designpatterns/abstractfactory/AbstractFactoryApplication.java
@@ -0,0 +1,14 @@
+package com.apal.designpatterns.abstractfactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbstractFactoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbstractFactoryApplication.class, args);
+	}
+
+}
+
