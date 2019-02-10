b8c116a04857e15a4c7e56ea64349394a7c38022
Andy <no@email>
2019-02-10 15:10:32 -0300

    README.md: CHain of Responsability Pattern | Theoretical Framework

---
+++ behavioral-patterns/chain-of-responsability/src/main/java/com/apal/designpatterns/chainofresponsability/ChainOfResponsabilityApplication.java
@@ -0,0 +1,14 @@
+package com.apal.designpatterns.chainofresponsability;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChainOfResponsabilityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChainOfResponsabilityApplication.class, args);
+	}
+
+}
+
---
+++ behavioral-patterns/chain-of-responsability/src/test/java/com/apal/designpatterns/chainofresponsability/ChainOfResponsabilityApplicationTests.java
@@ -0,0 +1,17 @@
+package com.apal.designpatterns.chainofresponsability;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ChainOfResponsabilityApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
