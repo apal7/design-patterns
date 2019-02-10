4b38544cf171a4131917f65398ff4565bba4927d
Andy <no@email>
2019-02-10 11:01:09 -0300

    README.md: Decorator Pattern | Theoretical Framework

---
+++ structural-patterns/decorator/src/main/java/com/apal/designpatterns/decorator/DecoratorApplication.java
@@ -0,0 +1,14 @@
+package com.apal.designpatterns.decorator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecoratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DecoratorApplication.class, args);
+	}
+
+}
+
