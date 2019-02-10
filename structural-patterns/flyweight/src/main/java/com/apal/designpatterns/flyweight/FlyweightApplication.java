ecf05c1b2fd75df45248794378d801bdb40e3340
Andy <no@email>
2019-02-10 12:25:48 -0300

    README.md: Flyweight Pattern | Theoretical Framework

---
+++ structural-patterns/flyweight/src/main/java/com/apal/designpatterns/flyweight/FlyweightApplication.java
@@ -0,0 +1,14 @@
+package com.apal.designpatterns.flyweight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlyweightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlyweightApplication.class, args);
+	}
+
+}
+
