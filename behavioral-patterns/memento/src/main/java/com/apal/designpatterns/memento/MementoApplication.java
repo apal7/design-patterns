c03e758673b4a2384d4a46d8f52734715291747f
Andy <no@email>
2019-02-11 10:14:35 -0300

    README.md: Memento Pattern | Theoretical Framework

---
+++ behavioral-patterns/memento/src/main/java/com/apal/designpatterns/memento/MementoApplication.java
@@ -0,0 +1,14 @@
+package com.apal.designpatterns.memento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MementoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MementoApplication.class, args);
+	}
+
+}
+
