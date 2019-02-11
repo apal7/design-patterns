d8d91a5eb0b9756e1aeb2bb0f2a612f86cbb8c8b
Andy <no@email>
2019-02-11 10:32:18 -0300

    README.md: Observer Pattern | Theoretical Framework

---
+++ behavioral-patterns/observer/src/main/java/com/apal/designpatterns/observer/ObserverApplication.java
@@ -0,0 +1,14 @@
+package com.apal.designpatterns.observer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObserverApplication.class, args);
+	}
+
+}
+
