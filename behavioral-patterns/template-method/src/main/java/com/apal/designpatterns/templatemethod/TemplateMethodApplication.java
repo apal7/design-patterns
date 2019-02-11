6e3c1fe7abba4d9c5b184a5fe1e8ed43fc138c3a
Andy <no@email>
2019-02-11 14:16:08 -0300

    README.md: Template Method | Theoretical Framework

---
+++ behavioral-patterns/template-method/src/main/java/com/apal/designpatterns/templatemethod/TemplateMethodApplication.java
@@ -0,0 +1,14 @@
+package com.apal.designpatterns.templatemethod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemplateMethodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemplateMethodApplication.class, args);
+	}
+
+}
+
---
+++ behavioral-patterns/template-method/src/test/java/com/apal/designpatterns/templatemethod/TemplateMethodApplicationTests.java
@@ -0,0 +1,17 @@
+package com.apal.designpatterns.templatemethod;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TemplateMethodApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
