d1d8d6bc6a9a76516ac4419dc2ac6e99f5fed194
Andy <no@email>
2019-02-12 15:30:07 -0300

    -Add: Composite Pattern | Simple Java Implementation

--- structural-patterns/composite/src/main/java/com/apal/designpatterns/composite/CompositeApplication.java
+++ structural-patterns/composite/src/main/java/com/apal/designpatterns/composite/CompositeApplication.java
@@ -1,14 +1,39 @@
 package com.apal.designpatterns.composite;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.apal.designpatterns.composite.editor.ImageEditor;
+import com.apal.designpatterns.composite.shape.Circle;
+import com.apal.designpatterns.composite.shape.Rectangle;
+import com.apal.designpatterns.composite.shape.CompoundShape;
+import com.apal.designpatterns.composite.shape.Dot;
 
-@SpringBootApplication
+
+import java.awt.*;
+
+
+
 public class CompositeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CompositeApplication.class, args);
+
+        ImageEditor editor = new ImageEditor();
+
+        editor.loadShapes(
+                new Circle(10, 10, 10, Color.BLUE),
+
+                new CompoundShape(
+                        new Circle(110, 110, 50, Color.RED),
+                        new Dot(160, 160, Color.RED)
+                ),
+
+                new CompoundShape(
+                        new Rectangle(250, 250, 100, 100, Color.GREEN),
+                        new Dot(240, 240, Color.GREEN),
+                        new Dot(240, 360, Color.GREEN),
+                        new Dot(360, 360, Color.GREEN),
+                        new Dot(360, 240, Color.GREEN)
+                )
+        );
+
 	}
 
 }
-
