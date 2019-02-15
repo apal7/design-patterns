73070d6e159b33f62fa1a8c6fac1aa3ab1ecdf00
Andy <no@email>
2019-02-15 12:11:23 -0300

    -Add: Template Method Pattern | Simple Java Implementation

--- behavioral-patterns/template-method/src/main/java/com/apal/designpatterns/templatemethod/TemplateMethodApplication.java
+++ behavioral-patterns/template-method/src/main/java/com/apal/designpatterns/templatemethod/TemplateMethodApplication.java
@@ -1,14 +1,42 @@
 package com.apal.designpatterns.templatemethod;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+import com.apal.designpatterns.templatemethod.network.Facebook;
+import com.apal.designpatterns.templatemethod.network.Network;
+import com.apal.designpatterns.templatemethod.network.Twitter;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class TemplateMethodApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TemplateMethodApplication.class, args);
-	}
+	public static void main(String[] args) throws IOException {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        Network network = null;
+        System.out.print("Input user name: ");
+        String userName = reader.readLine();
+        System.out.print("Input password: ");
+        String password = reader.readLine();
+
+        // Enter the message.
+        System.out.print("Input message: ");
+        String message = reader.readLine();
+
+        System.out.println("\nChoose social network for posting message.\n" +
+                "1 - Facebook\n" +
+                "2 - Twitter");
+        int choice = Integer.parseInt(reader.readLine());
+
+        // Create proper network object and send the message.
+        if (choice == 1) {
+            network = new Facebook(userName, password);
+        } else if (choice == 2) {
+            network = new Twitter(userName, password);
+        }
+        network.post(message);
+
+    }
 
 }
-
